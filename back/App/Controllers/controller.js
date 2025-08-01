3eba4e0119bb26d27b9526d2d9bad25e164abda8
ayushmishra950 <ayush950mishra@gmail.com>
2025-08-01 20:58:16 +0530

    Update controller.js

--- back/App/Controllers/controller.js
+++ back/App/Controllers/controller.js
@@ -41,15 +41,16 @@ const registerUser = async (req, res) => {
 };
 
 const loginUser = async (req, res) => {
+  console.log(req.body);
+  
   const { email, password } = req.body;
+  if(!email || !password){
+    return res.status(400).json({message:'All fields are required'})
+  } 
 
   try {
     const user = await User.findOne({ email });
     if (!user) return res.status(404).json({ message: 'User not found' });
-
-    if (!user.isVerified) {
-      return res.status(401).json({ message: 'Please verify OTP first' });
-    }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid password' });
@@ -61,7 +62,6 @@ const loginUser = async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 };
-
 
 const searchMovie = async (req, res) => {
     console.log(req.query);
